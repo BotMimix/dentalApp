4bd6ffd9e1614bb71eab10dec20e9537c0308720
mimi <96549901+BotMimix@users.noreply.github.com>
2023-11-30 12:58:30 +0100

    Cleaning up volumes & docker-compose command

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h2>Pedro is the cutest baby</h2>')
+  res.send(`
+  <h2>Pedro is the cutest baby</h2>
+  <p>i love my scooby</p>
+  `)
 });
 
 const port = process.env.PORT || 3000;
